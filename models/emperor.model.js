9d93df54b6718da47d427a14a8d39cb9a9158d81
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-30 18:13:01 -0500

    first commit

---
+++ models/emperor.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const emperorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    yearBorn: { type: Number, required: false },
+    riseToPower: { type: String, required: false },
+    riseToPowerYear: { type: Number, required: false },
+    death: { type: Number, required: false },
+    bio: { type: String, required: true },
+    accomplishments: { type: String, required: false },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Emperor = mongoose.model("Emperor", emperorSchema);
+
+module.export = Emperor;
